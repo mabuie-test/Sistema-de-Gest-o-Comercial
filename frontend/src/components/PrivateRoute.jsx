2ade659598836e13e860c39011978c2cd0fa7e09
mabuie-test <jorgemaabuie@gmail.com>
2025-05-14 18:59:51 +0200

    Update PrivateRoute.jsx

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,1 +1,10 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { getToken } from '../services/authService';
+
+function PrivateRoute() {
+  return getToken() ? <Outlet /> : <Navigate to="/login" />;
+}
+
+export default PrivateRoute;
 
