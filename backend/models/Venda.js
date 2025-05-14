59a68868cde539d6f1f381dc90b9d2d20075295e
mabuie-test <jorgemaabuie@gmail.com>
2025-05-14 18:28:32 +0200

    Update Venda.js

--- backend/models/Venda.js
+++ backend/models/Venda.js
@@ -1,1 +1,18 @@
+// models/Venda.js
+const mongoose = require('mongoose');
+
+const itemVendaSchema = new mongoose.Schema({
+  produto:   { type: mongoose.Schema.Types.ObjectId, ref: 'Produto', required: true },
+  quantidade:{ type: Number, required: true },
+  precoUnit: { type: Number, required: true },
+});
+
+const vendaSchema = new mongoose.Schema({
+  items:      [itemVendaSchema],
+  total:      { type: Number, required: true },
+  dataVenda:  { type: Date, default: Date.now },
+  user:       { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Venda', vendaSchema);
 
