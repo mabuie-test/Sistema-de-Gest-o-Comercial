939425001f5a8218d9fc2089accff0b80f9922d0
mabuie-test <jorgemaabuie@gmail.com>
2025-05-14 22:18:20 +0200

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,15 @@
 // routes/authRoutes.js
 const express = require('express');
-const { registar, login } = require('../controllers/authController');
+const {
+  registar,
+  login,
+  registerAdmin   // importar o novo controller
+} = require('../controllers/authController');
+
 const router = express.Router();
 
-router.post('/register', registar);
-router.post('/login', login);
+router.post('/register',       registar);
+router.post('/login',          login);
+router.post('/register-admin', registerAdmin);  // endpoint para criar admin
 
 module.exports = router;
-
