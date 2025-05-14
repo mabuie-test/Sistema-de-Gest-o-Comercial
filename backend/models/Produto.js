7bbc6829b881bb2e346a3452f575facd154d44d8
mabuie-test <jorgemaabuie@gmail.com>
2025-05-14 18:25:44 +0200

    Update Produto.js

--- backend/models/Produto.js
+++ backend/models/Produto.js
@@ -1,1 +1,12 @@
+// models/Produto.js
+const mongoose = require('mongoose');
+
+const produtoSchema = new mongoose.Schema({
+  nome:       { type: String, required: true },
+  preco:      { type: Number, required: true },
+  quantidade: { type: Number, required: true },
+  categoria:  { type: String, default: 'Geral' },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Produto', produtoSchema);
 
