b9d07addd57aa0e9ac9efabf4d833df7ae6ecbfa
mabuie-test <jorgemaabuie@gmail.com>
2025-05-14 18:33:29 +0200

    Update produtoRoutes.js

--- backend/routes/produtoRoutes.js
+++ backend/routes/produtoRoutes.js
@@ -1,1 +1,17 @@
+// routes/produtoRoutes.js
+const express = require('express');
+const auth = require('../middleware/authMiddleware');
+const {
+  listar, obter, criar, editar, apagar
+} = require('../controllers/produtoController');
+
+const router = express.Router();
+
+router.get('/', auth, listar);
+router.get('/:id', auth, obter);
+router.post('/', auth, criar);
+router.put('/:id', auth, editar);
+router.delete('/:id', auth, apagar);
+
+module.exports = router;
 
